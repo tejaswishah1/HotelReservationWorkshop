4f60c6078762fcd440cc3eb7bc413e3ce5fed395
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-05 13:46:50 +0530

    [Tejas] Initial Commit

---
+++ Hotel.cs
@@ -0,0 +1,33 @@
+﻿namespace HotelReservationSystemWorkshop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Hotel
+    {
+        /// <summary>
+        /// Declaring name of hotel
+        /// </summary>
+        string mNameOfHotel;
+        /// <summary>
+        /// Rate of Hotel
+        /// </summary>
+        int mRegularRate;
+        /// <summary>
+        /// Constructor to assign default values
+        /// </summary>
+        public Hotel()
+        {
+            this.mNameOfHotel = null;
+            this.mRegularRate = 0;
+        }
+        /// <summary>
+        /// Constructor to assign values given by user
+        /// </summary>
+        public Hotel(string name, int regularRate)
+        {
+            this.mNameOfHotel = name;
+            this.mRegularRate = regularRate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelReservationSystemWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hotel Reservation System");
+            ////Creating list to store values:
+            List<Hotel> hotels = new List<Hotel>();
+
+            ////Adding Hotel names
+            hotels.Add(new Hotel("Lakewood", 110));
+            hotels.Add(new Hotel("Bridgewood", 160));
+            hotels.Add(new Hotel("Ridgewood", 220));
+        }
+    }
+}
